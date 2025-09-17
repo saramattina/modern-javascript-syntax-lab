9b902c2326543bb588b2594594e6bb0e8f55a5b1
Sara Mattina <smattina@gmail.com>
2025-09-17 14:30:21 -0400

    exercise 9 complete

--- app.js
+++ app.js
@@ -145,5 +145,24 @@ function describeNoun(noun = "cat", adjective = "orange") {
    console.log(`The ${noun} is ${adjective}.`);
 }
 
-describeNoun();
+// describeNoun();
+
+// Exercise 9: Ternary Operator
+// a. Convert the following `if...else` statement into a ternary:
+//
+//    if (pizza === 'tasty') {
+//      console.log('yum');
+//    } else {
+//      console.log('yuck');
+//    }
+//
+// Starting code (don't modify this):
+
+const pizza = 'tasty';
+
+// Your code here:
+
+pizza === "tasty" ? console.log("yum") : console.log("yuck");
+
+
 
